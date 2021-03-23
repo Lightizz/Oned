8d7c059aaccdcacb0b66c73788f11ef42bd5aca7
Lightizz <53272877+Lightizz@users.noreply.github.com>
2021-03-23 19:42:26 +0100

    Splash screen bases

--- app/src/main/java/fr/lightiz/oned/SplashScreenActivity.kt
+++ app/src/main/java/fr/lightiz/oned/SplashScreenActivity.kt
@@ -1,11 +1,26 @@
 package fr.lightiz.oned
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import java.util.*
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    var homeActivity = MainActivity()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val handler = Handler()
+        handler.postDelayed({ goToMainPage() }, 1000)
+    }
+
+    private fun goToMainPage(){
+        val intent = Intent(applicationContext, homeActivity.javaClass)
+        startActivity(intent)
+        finish()
     }
 }
