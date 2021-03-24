b9532de8bd40793c561f2cecef53f6bba7653fd0
Lightizz <53272877+Lightizz@users.noreply.github.com>
2021-03-24 18:07:05 +0100

    Home page + splash update

--- app/src/main/java/fr/lightiz/oned/SplashScreenActivity.kt
+++ app/src/main/java/fr/lightiz/oned/SplashScreenActivity.kt
@@ -4,18 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 import java.util.*
 
 class SplashScreenActivity : AppCompatActivity() {
 
     var homeActivity = MainActivity()
 
+    private val splashScreenTime:Long = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash_screen)
 
         val handler = Handler()
-        handler.postDelayed({ goToMainPage() }, 1000)
+        handler.postDelayed({ goToMainPage() }, splashScreenTime)
     }
 
     private fun goToMainPage(){
